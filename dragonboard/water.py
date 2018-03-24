7f94b3b145ad172b37425a82c329f500eaac2a72
KevinAiken <aiken.kevin.m@gmail.com>
2018-03-24 16:14:12 -0400

    setup python script to read from serial on DragonBoard

--- dragonboard/water.py
+++ dragonboard/water.py
@@ -1,1 +1,11 @@
-print 'fuck'
+import requests
+import serial
+
+ser = serial.Serial('/dev/ttyACM0', 9600)
+
+print "Python script started"
+while True:
+    print ser.readline()
+    #needs to be parsed into accel values and capacitance
+    # if accelerometer in the z plane = around 1 g then record capacitance and send to server 
+
