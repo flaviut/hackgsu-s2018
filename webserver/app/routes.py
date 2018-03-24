60d7378c82eede7e8982ca3ba82c3ac1ca9ae3e5
Flaviu Tamas <tamasflaviu@gmail.com>
2018-03-24 14:32:14 -0400

    Improve site

--- webserver/app/routes.py
+++ webserver/app/routes.py
@@ -1,17 +1,67 @@
 # coding=utf-8
+import sqlite3
+from datetime import datetime
 
-from flask import render_template, flash, redirect, session, url_for, request, g, Markup
+from flask import request, g, jsonify
+
 from app import app
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
 
 
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
 
 
+@app.cli.command('initdb')
+def initdb_command():
+    """Initializes the database."""
+    init_db()
+    print('Initialized the database.')
+
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+
+@app.route('/log')
+def log():
+    db = get_db()
+    cur = db.execute(
+        'SELECT water_level,entry_time FROM entries ORDER BY entry_time DESC')
+    entries = cur.fetchall()
+
+    return jsonify([{"water_level": row[0], "time": row[1]} for row in entries])
+
+
+@app.route('/add', methods=['POST'])
+def add_record():
+    data = request.get_json(force=True)
+    db = get_db()
+    entry_value = round(data['water_level'] * 4) / 4.0
+    time = datetime.now()
+    db.execute(
+        "INSERT INTO entries (water_level, entry_time)"
+        "VALUES (?, ?)", [entry_value, time.isoformat()])
+    db.commit()
+    return jsonify({"level": entry_value, "time": time})
